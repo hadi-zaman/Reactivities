ff17fc70114ca46afe242e64e2cd13cc668b095f
hadi <hadi.cit.bd@gmail.com>
2026-02-13 19:34:47 +0600

    code updated

--- API/Program.cs
+++ API/Program.cs
@@ -11,25 +11,29 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
-using var scope =app.Services.CreateScope();
-var services =scope.ServiceProvider;
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
 try
 {
-    var context =services.GetRequiredService<AppDbContext>();
+    var context = services.GetRequiredService<AppDbContext>();
     await context.Database.MigrateAsync();
     await DbInitializer.SeedData(context);
 }
 catch (Exception ex)
 {
-    var logger =services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An error ocurred during migration.");
-    
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during migration.");
+
 }
 
 app.Run();
